25cb564059d2a5a6df3006d8a6ea8aa287173ef5
Unknown <benjamin.strabach@live.fr>
2019-01-23 14:53:28 +0100

    Correction taille image drapeau et extension

--- www/rechercheCarte.js
+++ www/rechercheCarte.js
@@ -523,9 +523,9 @@ function updatePrixRachat(idcarte) {
     var langue2 = tabdrap[lang - 1];
     var directory = 'http://www.counterspell.fr/affiche_prix_simple/' + idcarte + '/' + foil + '/' + etat + '/' + langue2 + '/echange/72000/rien';
     var element_idJson = 'storJson';
-    console.log(directory);
-    
+
     submitForm(element_idJson, directory, 'innerHTML');
+    
     var recup = document.getElementById(element_idJson).innerHTML;
     var prixRachat = recup * quant;
     if (prixRachat == '0.00' || prixRachat == 'NaN' || prixRachat == '0') {
